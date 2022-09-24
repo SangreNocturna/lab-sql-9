43e8bee43147186ba00c94d4a1ba4123abe58707
SangreNocturna <jtafernandez@gmail.com>
2022-09-24 11:29:59 +0200

    sql-9.ipynb

--- sql-9.sql
+++ sql-9.sql
@@ -23,7 +23,14 @@ ORDER BY length DESC
 LIMIT 10; 
 
 -- 5. How many films include Behind the Scenes content?
+SELECT count(film_id) FROM sakila.film
+WHERE special_features LIKE '%%Behind the Scenes%%';
 
+SELECT * FROM sakila.film;
+-- 6. Which kind of movies (rating) have a mean duration of more than two hours?
+SELECT rating, AVG(length) > 120 FROM sakila.film
+GROUP BY rating;
 
-
--- STILL IN PROGRESS, I NEED TO SLEEP
+-- 7. Rank films by length (filter out the rows that have nulls or 0s in length column). In your output, only select the columns title, length, and the rank.
+SELECT title, length, rental_rate FROM sakila.film
+ORDER BY length DESC;
